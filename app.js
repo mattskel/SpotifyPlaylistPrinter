77a17a4d40986b2935d10b9abde3c27e8324db3a
mattskel <mattskelley@hotmail.com>
2018-06-04 14:16:19 +0800

    adding a new track to a playlist via url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/main', function(req, res) {
 
 var client_id = 'a2ff816c53a341db849e76d58e57cd92';
 var client_secret = 'deb4764130d840ca863761a3e781877f';
-var scope = 'user-read-private user-read-email';
+var scope = 'user-read-private user-read-email playlist-modify-private playlist-modify-public';
 var redirect_uri = 'http://localhost:3000/callback';
 
 var code = '';
@@ -107,8 +107,39 @@ app.get('/playlist',function(req,res) {
   res.redirect('/#');
 })
 
-app.get('/myTest',function(req,res) {
-    console.log('HERE');
+//app.get('/myTest',function(req,res) {
+//    console.log('HERE');
+//});
+
+app.get('/newPlaylist', function(req,res) {
+  console.log('New Playlist');
+  
+//  POST https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks
+  
+  var access_token = 'BQCx8EEaI-WlGwgvmKOHORyfcjJAZpmCdy9I_nDy9u7Z5JUizCmC0TZfXsqklbqoMhO-nuQ6DQER-haRtdVWTY0EfgKpGjNMnCl_Vkrf4-pPQvL-E1dr1lzrQVbpg9e8VLPAtUWx3M4ToQfLntXK3hB3NhT5FCCYgDIC2CgpDtuOJ_3KVqszj991WhhhvxvWG2UbZ6_SU5g88FRjK7jjN7_eCeDgrywV3Q';
+  
+  var track_id = '4D7jMhi6n60TZi9MBuvdYk';
+  
+  var options = {
+//    url: 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks'
+    url: 'https://api.spotify.com/v1/users/mattskelley-au/playlists/7nOq1mWncPNUmI0tEP83g8/tracks',
+    qs: {
+      uris:"spotify:track:4iV5W9uYEdYUVa79Axb7Rh"
+    },
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true
+  }   
+  
+  request.post(options, function(error, response, body) {
+    console.log(response);
+    console.log(body);
+  });
+  
+//  request.post(options, function(error, response, body) {
+//    console.log(response);
+//  
+//  });      
+  
 });
 
 app.listen(3000);
