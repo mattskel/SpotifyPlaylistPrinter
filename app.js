8381d712bb140a63841f089ef9df4116e4ba7592
mattskel <mattskelley@hotmail.com>
2018-06-30 21:35:26 +0800

    remove rename test variables

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var querystring = require('querystring');
 var app = express();
 
 app.use('/', express.static(__dirname + '/'));
-app.use('/myTest', express.static(__dirname + '/test.html'));
+app.use('/playlists', express.static(__dirname + '/playlists.html'));
 app.use('/playlist', express.static(__dirname + '/playlist.html'));
-//app.use('/newPlaylist',express.static(__dirname + '/data.html'));
 
 app.get('/hello', function(req, res){
     res.send('hello');
@@ -24,9 +23,6 @@ var redirect_uri = 'http://localhost:3000/callback';
 
 var code = '';
 var access_token = '';
-
-//var global_string = 'my string';
-//var global_array = [];
 
 app.get('/login', function(req,res) {
     res.redirect('https://accounts.spotify.com/authorize?' +
@@ -59,48 +55,28 @@ app.get('/callback', function(req,res) {
     request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             access_token = body.access_token;
-//            var access_token = body.access_token;
             
             var options = {
                 url: 'https://api.spotify.com/v1/me/playlists',
                 headers: { 'Authorization': 'Bearer ' + access_token },
                 json: true
             }
-            request.get(options, function(error, response, body) {
-//                console.log(body);
-            });
-            
-            res.redirect('/myTest#' + 
+          
+            res.redirect('/playlists#' + 
                 querystring.stringify({
                     access_token: access_token,
                     message: 'HELLO_WORLD'
             }));
         }
     });
-    
-    
 //    res.redirect('/#');
-    
-});
-
-app.get('/playlists',function(req,res) {
-    
-    var options = {
-//        url: 'https://api.spotify.com/v1/me',
-        url: 'https://api.spotify.com/v1/me/playlists',
-        headers: { 'Authorization': 'Bearer ' + access_token },
-        json: true
-    }
-    
-    res.redirect('/myTest');
 });
 
 app.get('/playlist',function(req,res) {
-//  console.log(req);
   res.redirect('/#');
 })
 
-app.get('/newPlaylist', function(req,res) {
+app.get('/orderPlaylist', function(req,res) {
   console.log('New Playlist');
   
   var access_token = req.query.access_token;
@@ -153,8 +129,8 @@ app.get('/newPlaylist', function(req,res) {
       })
     })
   })
-  
-  res.redirect('/playlist#id=' + playlist_id + '&access_token=' + access_token);
+//  res.redirect('/playlist#id=' + playlist_id + '&access_token=' + access_token);
+  res.redirect('/playlists#access_token=' + access_token);
 });
 
 app.get('/test',function(req,res) {
